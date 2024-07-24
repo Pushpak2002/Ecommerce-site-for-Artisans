36afcf6a9942eba6a079b402bde785a0f3eeb70f
Pushpak2002 <kamblepushpak27@gmail.com>
2024-07-24 19:33:31 +0530

    Authenticate Correct USer

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -10,13 +10,15 @@ const router = express.Router();
 const User = require('../Models/User');
 const { default: mongoose } = require('mongoose');
 
-//Create a new user
-router.post('/', [
+//Create a new user no login required
+router.post('/Signup', [
     body('Name','Min name length is 3').isLength({min: 3}),
     body('UserName','Min Username length is 3').isLength({min: 3}),
     body('Mail','Enter a valid Email').isEmail(),
     body('Password','Min password length is 5').isLength({min: 5}),
   ],async (req,res) => {
+
+    //If there are errors, return bad request
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -41,8 +43,7 @@ router.post('/', [
     const salt = await bcrypt.genSalt(10);
     const secPass = await bcrypt.hash(req.body.Password, salt);
 
-    
-
+  
 
     //New user add
     user = await User.create({
@@ -57,13 +58,58 @@ router.post('/', [
       }
     })
     const authToken = jwt.sign(data,JWT_SECRET);
-    console.log(authToken);
     res.json(authToken);
     }
     catch(error){
-      console.error(error.message)
+      console.error(error.message);
+      res.status(500).send("Some Error Occured");
     }
     
 })
+
+//Login user ...
+
+
+router.post('/login', [
+  body('UserName','Username can not be black').exists(),
+  body('Password','Password can not be blank').exists(),
+],async (req,res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json({errors: errors.array()});
+    }
+
+
+    const {UserName,Password} = req.body;
+    try{
+      let user = await User.findOne({UserName});
+      if(!user)
+      {
+        return res.status(400).json({error: "User does not exist"});
+      }
+
+      const passwordComp = await bcrypt.compare(Password, user.Password);
+      if(!passwordComp)
+      {
+        console.log(passwordComp);
+        return res.status(400).json({error: "Wrong Password"});
+      }
+
+      const data = ({
+        user:{
+          id: user.id
+        }
+      })
+      const authToken = jwt.sign(data,JWT_SECRET);
+      res.json(authToken);
+    }
+    catch(error){
+      console.error(error.message);
+      res.status(500).send("Inter Error Occured");
+    }
+})
+
+
 
 module.exports = router
