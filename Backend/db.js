4bbc15e12157f7524ba46b19c3d80d8bcfc4f6e3
Pushpak2002 <kamblepushpak27@gmail.com>
2024-07-24 10:49:10 +0530

    MongoDB Connection

---
+++ Backend/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const mongoURL = 'mongodb://localhost:27017/';
+
+const connectToMongo = async () => {
+    try {
+      await mongoose.connect(mongoURL, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        
+      });
+      console.log('MongoDB database connection established successfully');
+      return true;
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      return false;
+    }
+  };
+
+module.exports = connectToMongo;
